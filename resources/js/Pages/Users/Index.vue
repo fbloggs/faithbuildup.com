4c80e272d1c219e53b3877ff5827cea2457a2615
Duncan Kenzie <djkenzie@gmail.com>
2025-09-18 15:35:06 -0700

    Converted to latest version of Inertia and to Vue 3 composition API

--- resources/js/Pages/Users/Index.vue
+++ resources/js/Pages/Users/Index.vue
@@ -1,5 +1,5 @@
 <template>
-  <app-layout>
+  <AppLayout>
     <template #header>
       <h2 class="font-semibold text-xl text-gray-800 leading-tight">
         Users List
@@ -9,120 +9,128 @@
       <div class="max-w-7xl mx-auto py-5 sm:px-6 lg:px-8">
         <div class="bg-white overflow-hidden shadow-xl p-2 sm:p-6 sm:rounded-lg">
           <div class="sm:mb-6 flex justify-between items-center">
-          <search-filter
-            v-model="form.search"
-            class="w-full max-w-md mr-4 text-sm"
-            @reset="reset"
-          ></search-filter>
-
+            <SearchFilter
+              v-model="form.search"
+              class="w-full max-w-md mr-4 text-sm"
+              @reset="reset"
+            />
           </div>
+          
           <table class="w-full whitespace-nowrap">
-            <tr class="text-left font-bold">
-              <th class="pt-4 sm:pr-6 sm:pb-2">User Id</th>
-              <th class="sm:pr-6 pt-4 sm:pb-2">Email</th>
-              <th class="sm:pr-6 pt-4 sm:pb-2">Name</th>
-              <th class="sm:pr-6 pt-4 sm:pb-2">Actions</th>
+            <thead>
+              <tr class="text-left font-bold">
+                <th class="pt-4 sm:pr-6 sm:pb-2">User Id</th>
+                <th class="sm:pr-6 pt-4 sm:pb-2">Email</th>
+                <th class="sm:pr-6 pt-4 sm:pb-2">Name</th>
+                <th class="sm:pr-6 pt-4 sm:pb-2">Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr
+                v-for="user in users.data"
+                :key="user.id"
+                class="hover:bg-gray-100 focus-within:bg-gray-100"
+              >
+                <td class="border-t c">
+                  {{ user.id }}
+                </td>
+                <td class="border-t c">
+                  {{ user.email }}
+                </td>
+                <td class="border-t leading-none sm:leading-tight text-sm">
+                  <Link
+                    class="py-4 flex items-center focus:text-indigo-500"
+                    :href="route('user.showquiz', user.id)"
+                  >
+                    {{ user.name }}
+                  </Link>
+                </td>
+                <td class="border-t c">
+                  <Link 
+                    v-if="user.quizDone" 
+                    class="btn-primary transition duration-300 ease-in-out focus:outline-none focus:shadow-outline bg-indigo-500 hover:bg-indigo-900 text-white font-normal py-2 px-4 mr-1 rounded inline-flex" 
+                    :href="route('quizzes.anotheruser', { 'quizid': 1, 'userid': user.id })"
+                  >
+                    <Icon name="eye" class="w-4 h-4 mr-2" />
+                    <span>Show Quiz</span>
+                  </Link>
 
-            </tr>
-            <tr
-              v-for="user in users.data"
-              :key="user.id"
-              class="hover:bg-gray-100 focus-within:bg-gray-100"
-            >
-            <td class="border-t c">
-                {{ user.id }}
-            </td>
-              <td class="border-t c">
-                {{ user.email }}
-              </td>
-              <td class="border-t leading-none sm:leading-tight text-sm">
-                <inertia-link
-                  class="py-4 flex items-center focus:text-indigo-500"
-                  :href="route('user.showquiz', user.id)"
-                >
-                  {{ user.name }}
-                </inertia-link>
-              </td>
-              <td class="border-t c">
-                  <inertia-link v-if="user.quizDone" class="btn-primary transition duration-300 ease-in-out focus:outline-none focus:shadow-outline bg-indigo-500 hover:bg-indigo-900 text-white font-normal py-2 px-4 mr-1 rounded inline-flex" :href="route('quizzes.anotheruser', { 'quizid' : 1, 'userid' : user.id})">
-                      <icon name="eye" class="w-4 h-4 mr-2"></icon>
-                      <span>Show Quiz</span>
-                      </inertia-link>
-
-                            <inertia-link v-if="user.timeLineDone" class="btn-primary transition duration-300 ease-in-out focus:outline-none focus:shadow-outline bg-indigo-500 hover:bg-indigo-900 text-white font-normal py-2 px-4 mr-1 rounded inline-flex" :href="route('timelines.anotheruser', user.id)">
-                      <icon name="eye" class="w-4 h-4 mr-2"></icon>
-                      <span>Show Timeline</span>
-                      </inertia-link>
-              </td>
-            </tr>
-            <tr v-if="users.data.length === 0">
-              <td class="border-t px-6 py-4" colspan="4">No Accounts found.</td>
-            </tr>
+                  <Link 
+                    v-if="user.timeLineDone" 
+                    class="btn-primary transition duration-300 ease-in-out focus:outline-none focus:shadow-outline bg-indigo-500 hover:bg-indigo-900 text-white font-normal py-2 px-4 mr-1 rounded inline-flex" 
+                    :href="route('timelines.anotheruser', user.id)"
+                  >
+                    <Icon name="eye" class="w-4 h-4 mr-2" />
+                    <span>Show Timeline</span>
+                  </Link>
+                </td>
+              </tr>
+              <tr v-if="users.data.length === 0">
+                <td class="border-t px-6 py-4" colspan="4">No Accounts found.</td>
+              </tr>
+            </tbody>
           </table>
 
-          <pagination :links="users.links" />
+          <Pagination :links="users.links" />
         </div>
       </div>
     </div>
-  </app-layout>
+  </AppLayout>
 </template>
-     <style>
-</style>
-    <script>
-import AppLayout from "@/Layouts/AppLayout.vue";
-import Pagination from "@/Shared/Pagination.vue";
-import Icon from "@/Shared/Icon.vue";
 
-import pickBy from "lodash/pickBy";
-import throttle from "lodash/throttle";
-import mapValues from "lodash/mapValues";
-import SearchFilter from "@/Shared/SearchFilter.vue";
+<script setup>
+import { reactive, watch } from 'vue'
+import { Link, router } from '@inertiajs/vue3'
 
-export default {
-  components: {
-    AppLayout,
-    Pagination,
-    Icon,
-    pickBy,
-    throttle,
-    mapValues,
-    SearchFilter,
-  },
+import AppLayout from '@/Layouts/AppLayout.vue'
+import Pagination from '@/Shared/Pagination.vue'
+import Icon from '@/Shared/Icon.vue'
+import SearchFilter from '@/Shared/SearchFilter.vue'
 
-  props: {
-    userid: Number,
-    users: Object,
-    filters:  [Array, Object],
-    errors: Object,
-  },
+import pickBy from 'lodash/pickBy'
+import throttle from 'lodash/throttle'
+import mapValues from 'lodash/mapValues'
 
-  data() {
-    return {
-      form: {
-        search: this.filters.search,
-      },
-    };
-  },
+// Define props using defineProps
+const props = defineProps({
+  userid: Number,
+  users: Object,
+  filters: [Array, Object],
+  errors: Object,
+})
 
-  watch: {
-    form: {
-      handler: throttle(function () {
-        let query = pickBy(this.form);
-        this.$inertia.replace(
-          this.route(
-            "users.index",
-            Object.keys(query).length ? query : { remember: "forget" }
-          )
-        );
-      }, 150),
-      deep: true,
-    },
-  },
-  methods: {
-    reset() {
-      this.form = mapValues(this.form, () => null);
-    },
-  },
-};
+// Reactive form data
+const form = reactive({
+  search: props.filters?.search || null,
+})
+
+// Watch for form changes with throttling
+watch(
+  form,
+  throttle(() => {
+    const query = pickBy(form)
+    router.visit(
+      route(
+        'users.index',
+        Object.keys(query).length ? query : { remember: 'forget' }
+      ),
+      {
+        preserveState: true,
+        replace: true,
+      }
+    )
+  }, 150),
+  { deep: true }
+)
+
+// Methods
+const reset = () => {
+  Object.assign(form, mapValues(form, () => null))
+}
 </script>
 
+<style>
+.c {
+  @apply px-6 py-4;
+}
+</style>
